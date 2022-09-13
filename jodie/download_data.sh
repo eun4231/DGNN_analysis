7f1400e7e8caf838cc822dc862312cc861405160
Yahya-alhinai <37601653+Yahya-alhinai@users.noreply.github.com>
2022-09-12 20:50:01 -0400

    Update Jodie

--- jodie/download_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget http://snap.stanford.edu/jodie/reddit.csv -P data/
-wget http://snap.stanford.edu/jodie/wikipedia.csv -P data/
-wget http://snap.stanford.edu/jodie/mooc.csv -P data/
-wget http://snap.stanford.edu/jodie/lastfm.csv -P data/
